dc99b9824ab244b67cdb5005b7f545e716e51ea4
Apoorva Thumma <apoorva.thumma79@gmail.com>
2021-11-15 13:59:08 -0500

    more dependencies

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "KitsRealtyLambdaFunction2": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "KitsRealtyAPI2": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
